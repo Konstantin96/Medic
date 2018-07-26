f4f866fa8a0dfba5505507ea52f0d15c701bb0e3
Konstatin <34293528+Konstantin96@users.noreply.github.com>
2018-07-26 12:04:56 +0600

    Добавьте файлы проекта.

---
+++ EgovMedical/Program.cs
@@ -0,0 +1,51 @@
+﻿using Egov.Medical.Lib.Model;
+using GeneratorName;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EgovMedical.Model;
+
+namespace Egov.Medical
+{
+
+    
+  
+    class Program
+    {
+        Discount d = Discount.name;
+        //enum
+        Console.WriteLine(Enum.GetUnderlyingType();
+       
+
+
+        static void Main(string[] args)
+        {
+            ServiseProgramm.PrintMenu();
+
+            switch (ServiseProgramm.getPunctMenu())
+            {
+                case 1:
+                    {
+                        ServiseProgramm.Autorisation();
+                    }
+                    break;
+                case 2:
+                    {
+                        if (ServiseUser.Registration(ServiseProgramm.GetUserInfoForRegistration()))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("register ok");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("register error");
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
